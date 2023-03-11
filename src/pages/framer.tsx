8356393b1b0e73fc8a9801abdad88f2ad43ed4a3
Luca <lucafrederice@gmail.com>
2023-03-11 19:54:21 -0300

    changes

--- src/pages/framer.tsx
+++ src/pages/framer.tsx
@@ -89,7 +89,7 @@ export const App = () => {
                         ref={header}
                         className="w-full grid justify-items-center content-center gap-5 pt-3 px-4"
                         style={{
-                            paddingBottom: "calc(env(safe-area-inset-bottom) + 1rem)"
+                            paddingBottom: "calc(env(safe-area-inset-bottom) + 1.5rem)"
                         }}
                     >
                         <div className="w-1/4 max-w-[8rem] h-1 rounded bg-gray-300" />
@@ -106,14 +106,17 @@ export const App = () => {
             </div>
 
             <div
-                className={status ? "fixed inset-0 w-full h-screen transition-all ease-in-out duration-200 bg-black bg-opacity-60 backdrop-blur-sm" : ""}
+                className={status ? "fixed inset-0 w-full transition-all ease-in-out duration-200 bg-black bg-opacity-60 backdrop-blur-sm" : ""}
+                        style={{
+                            height: globalThis?.window?.innerHeight
+                        }}
             >
                 <motion.div
                     onLayoutAnimationStart={() => setisAnimating(true)}
                     onLayoutAnimationComplete={() => setisAnimating(false)}
                     className={`fixed ${status ? "md:inset-60 inset-8 bg-pink-600" : "bg-amber-500 bottom-5 left-5 w-32 h-10"} ${isOpening() ? "rounded-xl" : opened() ? "rounded-xl" : ""} ${isClosing() ? "rounded-[1px]" : closed() ? "rounded" : ""}`}
                     style={{
-                        paddingBottom: "calc(env(safe-area-inset-bottom) + 1rem)"
+                        paddingBottom: "calc(env(safe-area-inset-bottom) + 1.5rem)"
                     }}
                     transition={{
                         x: {
